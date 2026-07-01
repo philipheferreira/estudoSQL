bd2f0692f980b225adbfa2d5f45b9b1bbc10b4e0
philipheferreira <philipheferreira@gmail.com>
2026-07-01 16:44:30 -0300

    add things i forget

--- 003 - Comandos impressidiveis/001 - Tipos nulos e inteiros.sql
+++ 003 - Comandos impressidiveis/001 - Tipos nulos e inteiros.sql
@@ -7,6 +7,12 @@ SHOW TABLES;
 
 DESC cliente;
 
+EXEC sp_help 'CLIENTE'; /* comando equivalente ao DESC cliente */
+
+SELECT * FROM CLIENTE;
+
+/* sintaxe basica de insercao - INSERT INTO TABELA... */
+
 /* FORMA 01 - EXCLUINDO AS COLUNAS */
 
 INSERT INTO cliente VALUES ('Joao', 'M', 'joao@gmail.com', 988638273 , '22923110' , 'Maia Lacerda - Estacio - Rio De Janeiro - RJ');
@@ -17,4 +23,8 @@ INSERT INTO cliente VALUES ('Jorge', 'M', NULL , 885755896 , '58748895' , 'Oscar
 
 /* FORMA 02 - COLOCANDO AS COLUNAS */
 
-INSERT INTO cliente(nome, sexo, endereco, telefone, cpf) VALUES ('');
+INSERT INTO cliente(nome, sexo, endereco, telefone, cpf) VALUES ('Lilian', 'F', 'Senador Soares - Tijuca - Rio de Janeiro - RJ', '947785696', 887774856);
+
+/* FORMA 03 - INSERT COMPACTO - SOMENTE MYSQL */
+
+INSERT INTO CLIENTE VALUES('Ana', 'F', 'ana@globo.com', 85548962, '548556985' , 'Pres Antonio Carlos - Centro - Sao Paulo - SP'), ('Carla', 'F', 'carla@terati.com.br', 7745828, '66587458' , 'Samuel Silva - Centro - Belo Horizonte - MG');
